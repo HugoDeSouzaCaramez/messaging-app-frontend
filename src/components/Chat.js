b10123f24f0a891e27114e2859da4c1bb369edcd
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-29 22:34:26 -0300

    Criando um componente de rodapé de bate-papo

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import {
+  AttachFile,
+  MoreVert,
+  SearchOutlined,
+  InsertEmoticon,
+} from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic';
 import './Chat.css';
 const Chat = () => {
   const [seed, setSeed] = useState('');
@@ -47,7 +53,14 @@ b${seed}.svg`}
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 };
