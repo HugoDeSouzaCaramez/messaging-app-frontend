47469bffaee0d3adaac7e5060a30d7ef7527de0c
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-12-03 18:12:11 -0300

    Usando Redux e API de Contexto
    redirecionando usuário ao logar com sucesso

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import { auth, provider } from '../firebase';
 import './Login.css';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
+
 const Login = () => {
-  const signIn = () => {};
+  const [{}, dispatch] = useStateValue();
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <div className="login__container">
